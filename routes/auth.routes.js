6b337cd7fe63ddd70267dd4b91ad6cd86655262f
Denys Novosad <denisnovosad3@gmail.com>
2023-10-30 16:12:36 +0200

    4 correct

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -14,12 +14,17 @@ router.post("/login", async (req, res) => {
         console.log(username,typeof username)
         if (username!== test) {
             console.log('hello')
-            return res.status(404).json({ message: "User not found", login: false });
+            return res.status(404)
+                .set("Access-Control-Allow-Origin", "*")
+                .set("Access-Control-Allow-Headers", "Content-Type")
+                .json({ message: "User not found", login: false });
         }
         //const isPassValid = bcrypt.compareSync(password, user.password);
       else  if (username===test && password !==testPassword) {
             return res
                 .status(400)
+                .set("Access-Control-Allow-Origin", "*")
+                .set("Access-Control-Allow-Headers", "Content-Type")
                 .json({ message: "Invalid password", password: false });
         }
         console.log("success");
@@ -28,7 +33,10 @@ router.post("/login", async (req, res) => {
              process.env.SECRET_KEY,
             { expiresIn: "1h" }
         );*/
-        return res.json({
+        return res
+            .set("Access-Control-Allow-Origin", "*")
+            .set("Access-Control-Allow-Headers", "Content-Type")
+            .json({
             success: true,
 
             user: {
@@ -36,8 +44,7 @@ router.post("/login", async (req, res) => {
                 username: test,
                 password: testPassword,
             },
-        }).set("Access-Control-Allow-Origin", "*")
-            .set("Access-Control-Allow-Headers", "Content-Type");
+        });
     } catch (e) {
         res.send({ message: "Server error" });
     }
