ac9b34d01686ac9a4a677d603d6cb9c9b214172e
Denys Novosad <denisnovosad3@gmail.com>
2023-10-31 22:49:32 +0200

    6 vercel

--- index.js
+++ index.js
@@ -11,13 +11,15 @@ const app=express()
 
 const PORT=process.env.SERVER_PORT
 const DB_URL=process.env.DB_URL
-const corsOptions = {
-    origin: 'https://fronttest-7k6j.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    optionsSuccessStatus: 204,
-};
 
-app.use(cors(corsOptions));
+
+app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://fronttest-7k6j.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use("/api/auth", authRouter);
