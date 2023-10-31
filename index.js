c02528aac9326c83ad2fc081e10c10fb83fed3e7
Denys Novosad <denisnovosad3@gmail.com>
2023-10-31 22:37:10 +0200

    5 vercel

--- index.js
+++ index.js
@@ -11,11 +11,17 @@ const app=express()
 
 const PORT=process.env.SERVER_PORT
 const DB_URL=process.env.DB_URL
+const corsOptions = {
+    origin: 'https://fronttest-7k6j.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    optionsSuccessStatus: 204,
+};
 
+app.use(cors(corsOptions));
 app.use(express.json())
 app.use(morgan('tiny'))
-app.use("/api/auth",cors(), authRouter);
-app.use("/api",cors(), clientsRouter);
+app.use("/api/auth", authRouter);
+app.use("/api", clientsRouter);
 
 
 function start(){
